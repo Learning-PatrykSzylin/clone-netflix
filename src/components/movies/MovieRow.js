ebbc85fd19cd3d45c0e48fa963a5ece27f87a937
Patryk <patryk.szylin.dev@gmail.com>
2021-04-08 18:00:22 +0100

    added carousel to movie strips

--- src/components/movies/MovieRow.js
+++ src/components/movies/MovieRow.js
@@ -2,6 +2,14 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../../context/GlobalState";
 import MovieItem from "./MovieItem";
 import "./MovieRow.scss";
+import Carousel from "react-elastic-carousel";
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 6, itemsToScroll: 6 },
+];
 
 function MovieRow({ movies }) {
   const { selectMovie, unselectMovie } = useContext(GlobalContext);
@@ -10,14 +18,20 @@ function MovieRow({ movies }) {
     <section className="sliderContent lolomoRow">
       <h1 className="rowHeader">Trending Now</h1>
       <div className="rowContainer">
-        {movies.map((movie) => (
-          <MovieItem
-            key={`${movie.id.toString()}`}
-            movie={movie}
-            onItemHover={selectMovie}
-            onItemUnselected={unselectMovie}
-          />
-        ))}
+        <Carousel
+          breakPoints={breakPoints}
+          itemPadding={[0, 0]}
+          showArrows={false}
+        >
+          {movies.map((movie) => (
+            <MovieItem
+              key={`${movie.id}`}
+              movie={movie}
+              onItemHover={selectMovie}
+              onItemUnselected={unselectMovie}
+            />
+          ))}
+        </Carousel>
       </div>
     </section>
   );
